65949add70c533f2633f089e0fcc28b36059a465
Leone Oliveira <falecom@leone.tec.br>
2021-08-06 00:25:35 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:15,
+  responsiveClass:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    300:{
+      items:2
+    },
+    500:{
+      items:3
+    },
+    700:{
+      items:4
+    },
+    900:{
+      items:5
+    }
+  }
+})
